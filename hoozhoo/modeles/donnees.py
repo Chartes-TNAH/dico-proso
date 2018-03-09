96bf1f08de9de05b408a49cb0db79afab142f518
Alix Chagué <alix.chague@enc-sorbonne.fr>
2018-03-09 10:25:54 +0100

    fusion des 2 ensembles de modeles

--- hoozhoo/modeles/donnees.py
+++ hoozhoo/modeles/donnees.py
@@ -1,7 +1,25 @@
-# fichier recevant les modèles de données pour l'application, hors données utilisateurs.
-
+from flask import url_for
+import datetime
 # ATTENTION
-# Ici on utilise la variable "db", comme dans le cours. Il faut vérifier qu'elle est bien déclarée là où elle doit l'être (app.py ?).
+# Ici on utilise la variable "db", comme dans le cours.
+# Il faut vérifier qu'elle est bien déclarée là où elle doit l'être (dans app.py ?).
+from .. app import db
+
+
+class Link(db.Model):
+    __tablename__ = "link"
+    link_id = db.Column(db.Integer, nullable=True, autoincrement=True, primary_key=True)
+    link_person1_id = db.Column(db.Integer, db.ForeignKey('person.person_id'))
+    link_person2_id = db.Column(db.Integer, db.ForeignKey('person.person_id'))
+    link_relation_type_id = db.Column(db.Integer, db.ForeignKey('relation_type.relation_type_id'))
+
+class Authorship_link(db.Model):
+    __tablename__ = "authorship_link"
+    authorship_link_id = db.Column(db.Integer, nullable=True, autoincrement=True, primary_key=True)
+    authorship_link_user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    authorship_link_link_id = db.Column(db.Integer, db.ForeignKey('link.link_id'))
+    link_relation_type_id = db.Column(db.Integer, db.ForeignKey('relation_type.relation_type_id'))
+    authorship_link_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
 class Person(db.Model):
     person_id = db.Column(db.Integer, unique=True, nullable=False, autoincrement=True, primary_key=True)
