ceeece001e332f2899c63acc7ebc70609b35ed4f
aureliavasile <aurelia.vasile@gmail.com>
2018-03-18 15:06:42 +0200

    remise des commentaires pour la partie test

--- hoozhoo/constantes.py
+++ hoozhoo/constantes.py
@@ -8,12 +8,13 @@ if SECRET_KEY == "JE SUIS UN SECRET !":
 
 
 # Partie tirée de l'exemple 18 de l'application gazetteer, à corriger !
+"""
 class _TEST:
     SECRET_KEY = SECRET_KEY
     # On configure la base de données
     SQLALCHEMY_DATABASE_URI = 'mysql://hoozhoo_user:password@localhost/hoozhoo'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+"""
 
 class _PRODUCTION:
     SECRET_KEY = SECRET_KEY
