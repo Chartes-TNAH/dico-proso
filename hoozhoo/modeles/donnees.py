3c37d7a6d2a45f4f62c0af6c978d68e2ee6e53ef
Alix Chagué <alix.chague@enc-sorbonne.fr>
2018-03-31 19:29:49 +0200

    correction de la table authorship

--- hoozhoo/modeles/donnees.py
+++ hoozhoo/modeles/donnees.py
@@ -336,7 +336,6 @@ class Authorship_link(db.Model):
     authorship_link_id = db.Column(db.Integer, nullable=True, autoincrement=True, primary_key=True)
     authorship_link_user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
     authorship_link_link_id = db.Column(db.Integer, db.ForeignKey('link.link_id'))
-    link_relation_type_id = db.Column(db.Integer, db.ForeignKey('relation_type.relation_type_id'))
     authorship_link_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 # Jointure
     user_link = db.relationship("User", back_populates="author_link")
