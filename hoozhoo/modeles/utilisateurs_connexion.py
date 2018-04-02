c9a0d6eaa296ff967fe133086b272ce570908ee9
Ekabat <batova.ekaterina@inbox.ru>
2018-04-03 00:19:11 +0200

    Conexxion, template, methode static

---
+++ hoozhoo/modeles/utilisateurs_connexion.py
@@ -0,0 +1,13 @@
+    @staticmethod
+    def identification(login, motdepasse):
+        """ Identifie un utilisateur. Si cela fonctionne, renvoie les données de l'utilisateurs.
+
+        :param login: Login de l'utilisateur
+        :param motdepasse: Mot de passe envoyé par l'utilisateur
+        :returns: Si réussite, données de l'utilisateur. Sinon None
+        :rtype: User or None
+        """
+        utilisateur = User.query.filter(User.user_login == login).first()
+        if utilisateur and check_password_hash(utilisateur.user_password, motdepasse):
+            return utilisateur
+        return None
---
+++ hoozhoo/routes_batova.py
@@ -0,0 +1,22 @@
+@app.route("/connexion", methods=["POST", "GET"])
+def connexion():
+    """ Route gérant les connexions
+    """
+    if current_user.is_authenticated is True:
+        flash("Vous êtes déjà connecté-e", "info")
+        return redirect("/")
+    # Si on est en POST, cela veut dire que le formulaire a été envoyé
+    if request.method == "POST":
+        user = User.identification(
+            login=request.form.get("login", None),
+            password=request.form.get("password", None)
+        )
+        if user:
+            flash("Connexion effectuée", "success")
+            login_user(user)
+            return redirect("/")
+        else:
+            flash("Les identifiants n'ont pas été reconnus", "error")
+
+    return render_template("pages/connexion.html")
+login.login_view = 'connexion'
